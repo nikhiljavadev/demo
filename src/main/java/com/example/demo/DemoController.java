31861aa8586da0d98e6fe6c05c4dcc02d4231cc7
Nikhil Masam <nikhil.javadev65@gmail.com>
2025-12-19 21:44:48 +0530

    demo removed the comment

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	// temp change
 	
 	@GetMapping("message")
 	public String getMessage() {
